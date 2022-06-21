3351e2d9d90e29269bcc087db805720f38161ee2
matheus <matheus.massari@protonmail.com>
2022-06-21 00:06:24 -0300

    packages added api: bcryptjs, jsonwebtoken e concurrently e suas respectivas configurações

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -23,6 +25,7 @@ const UserSchema = new mongoose.Schema({
         required: [true, "Please provide a password"],
         minlength: 6,
         maxlength: 20,
+        select: false,
     },
     lastName: {
         type: String,
@@ -38,5 +41,16 @@ const UserSchema = new mongoose.Schema({
         trim: true,
     },
 });
+
+UserSchema.pre("save", async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });
+};
 
 export default mongoose.model("User", UserSchema);
