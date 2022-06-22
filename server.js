b9b071c578e93d7398b5676538b53de1638d97d3
matheus <matheus.massari@protonmail.com>
2022-06-22 17:55:16 -0300

    frontend: proxy configurado em next-config

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-dotenv.config()
-import "express-async-errors"
+dotenv.config();
+import "express-async-errors";
 
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 
 // Routers
-import authRouter from "./routes/authRoutes.js"
+import authRouter from "./routes/authRoutes.js";
 import jobsRouter from "./routes/jobsRoutes.js";
 
 // Middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-app.use(express.json())
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Bem-vindo!");
+    res.json({ msg: "Bem vindo!" });
 });
 
-app.use("/api/v1/auth", authRouter)
+app.get("/api/v1", (req, res) => {
+    res.json({ msg: "Bem vindo a API!" });
+});
+
+app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
@@ -31,13 +36,13 @@ const port = process.env.PORT || 4000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL);
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}...`);
         });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-start()
+start();
