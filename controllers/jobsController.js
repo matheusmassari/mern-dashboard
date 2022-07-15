3bf1ab1a0edd5d6c05f800c057672e806a93e20b
matheus <matheus.massari@protonmail.com>
2022-07-15 10:08:04 -0300

    Back end: jobs controller setup INICIAL

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -10,13 +10,18 @@ const createJob = async (req, res) => {
     }
 
     req.body.createdBy = req.body.userId;
-    
+
     const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({ job });
 };
 
 const getAllJobs = async (req, res) => {
-    res.send("all job");
+    const jobs = await Job.find({ createdBy: req.user.userId });
+    res.status(StatusCodes.OK).json({
+        jobs,
+        totalJobs: jobs.length,
+        numOfPages: 1,
+    });
 };
 
 const deleteJob = async (req, res) => {
